36958cf23c3f5db05c5caffac5051a4b99eacfe5
momeemt <momiimt@gmail.com>
2020-04-24 10:59:38 +0900

    ✨ TopicとDropに文字数制約とnil制約を追加

--- app/models/drop.rb
+++ app/models/drop.rb
@@ -1,3 +1,5 @@
 class Drop < ApplicationRecord
   belongs_to :user
+  validates :content, presence: true, length: { in: 1..280 }
+  validates :user_id, presence: true
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,2 +1,6 @@
 class Topic < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, length: { in: 1..30 }
+  validates :content, presence: true, length: { minimum: 140 }
+  validates :user_id, presence: true
 end
