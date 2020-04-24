3e35058b959ddc18a12c494169dc08b75f1e9e8c
momeemt <momiimt@gmail.com>
2020-04-24 11:02:33 +0900

    ✨ パスワードの更新に対応するため、nil制約を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   has_one :user_job, dependent: :destroy
   has_one :optional_user_datum, dependent: :destroy
   has_many :drops, dependent: :destroy
+  has_many :topics, dependent: :destroy
   has_many :convenience_links, dependent: :destroy
+  has_many :notifications, dependent: :destroy
+  has_many :reports
   has_one_attached :icon
   has_one_attached :header
   before_save { self.email = email.downcase }
@@ -11,6 +14,6 @@ class User < ApplicationRecord
   validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   VALID_USER_ID_REGEX = /\A[a-zA-Z0-9]+\z/i
   validates :user_id, presence: true, length: { in: 3..15 }, format: { with: VALID_USER_ID_REGEX }, uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
   has_secure_password
 end
