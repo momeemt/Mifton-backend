bfaedf5a67d553e1f249cd46b4cc50446525ae9a
Momeemt <momiimt@gmail.com>
2020-06-04 20:36:49 +0900

    🚧 指定したアクセス以外を制限

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include SessionsHelper
+  include ActionController::HttpAuthentication::Token::ControllerMethods
+  before_action :authenticate
+  def authenticate
+    authenticate_or_request_with_http_token do |token, options|
+      token == ENV.fetch('MIFTON_API_TOKEN')
+    end
+  end
 end
